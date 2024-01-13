d228603eeaea4281114fb9a0090e86ba5835615e
Tejas Lale <tejaslale29@gmail.com>
2024-01-13 09:00:47 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+       int money = 2000;
+    System.out.println("Tejas lale");
+    System.out.println("LALE");
+    System.out.println(money);
+    money = 3000;
+       System.out.println(money);
+       int a = 121;
+       float f = 23.02F;
+       System.out.println("lll"+f );
+       System.out.println( "tejas"+a );
+   }
+}
